9b58d18676df07564dd26eb319838e96a6c09544
Bartłomiej Mazur <ab.mazur@gmail.com>
2021-03-29 18:31:25 +0200

    Language template

--- Templates/LanguageTemplate/LanguageTemplate.tsx
+++ Templates/LanguageTemplate/LanguageTemplate.tsx
@@ -1,59 +1,46 @@
 import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import Form from '../../components/Form/Form';
-import FormBigInput from '../../components/FormBigInput/FormBigInput';
+import FormLangInput from '../../components/FormLangInput/FormLangInput';
+import FormSmallInput from '../../components/FormSmallInput/FormSmallInput';
 
 interface LanguageProps {
-  importandFeatures: string;
-  differenceCompetitor: string;
-  futurePromotions: string;
+  chosenLanguage: string;
+  transactionId: string;
 }
 
 const LanguageTemplate = () => {
   const { register, watch } = useForm();
 
-  const storageTeam: LanguageProps = {
-    importandFeatures: '',
-    differenceCompetitor: '',
-    futurePromotions: '',
+  const storageLanguage: LanguageProps = {
+    chosenLanguage: '',
+    transactionId: '',
   };
 
   useEffect(() => {
-    Object.keys(storageTeam).forEach((key) => {
-      storageTeam[key] = localStorage.getItem(key) || '';
+    Object.keys(storageLanguage).forEach((key) => {
+      storageLanguage[key] = localStorage.getItem(key) || '';
       console.log(key);
     });
-    console.log(storageTeam);
+    console.log(storageLanguage);
   });
 
   return (
     <Form onSubmit={undefined}>
-      <FormBigInput
+      <FormLangInput
         register={register}
         fieldId="importandFeatures"
         fieldName="importandFeatures"
         label="What are the most importand things in your project that your future customer NEED to know?"
-        type="big"
-        value={storageTeam.importandFeatures}
-        rows={6}
+        type="small"
+        value={storageLanguage.chosenLanguage}
       />
-      <FormBigInput
-        register={register}
-        fieldId="differenceCompetitor"
-        fieldName="differenceCompetitor"
-        label="What's the difference between your project and closest competitor?"
-        type="big"
-        value={storageTeam.differenceCompetitor}
-        rows={6}
-      />
-      <FormBigInput
+      <FormSmallInput
         register={register}
         fieldId="futurePromotions"
         fieldName="futurePromotions"
         label="Future promotions / sales / air drops?"
-        type="big"
-        value={storageTeam.futurePromotions}
-        rows={6}
+        value={storageLanguage.transactionId}
       />
     </Form>
   );
