60e68d6136959194098b5761541e99c359069931
Bartłomiej Mazur <ab.mazur@gmail.com>
2021-03-29 18:19:45 +0200

    Features Template

---
+++ Templates/FeaturesTemplate/FeaturesTemplate.tsx
@@ -0,0 +1,62 @@
+import { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import Form from '../../components/Form/Form';
+import FormBigInput from '../../components/FormBigInput/FormBigInput';
+
+interface FeaturesProps {
+  importandFeatures: string;
+  differenceCompetitor: string;
+  futurePromotions: string;
+}
+
+const FeaturesTemplate = () => {
+  const { register, watch } = useForm();
+
+  const storageExampleUse: FeaturesProps = {
+    importandFeatures: '',
+    differenceCompetitor: '',
+    futurePromotions: '',
+  };
+
+  useEffect(() => {
+    Object.keys(storageExampleUse).forEach((key) => {
+      storageExampleUse[key] = localStorage.getItem(key) || '';
+      console.log(key);
+    });
+    console.log(storageExampleUse);
+  });
+
+  return (
+    <Form onSubmit={undefined}>
+      <FormBigInput
+        register={register}
+        fieldId="importandFeatures"
+        fieldName="importandFeatures"
+        label="What are the most importand things in your project that your future customer NEED to know?"
+        type="big"
+        value={storageExampleUse.importandFeatures}
+        rows={6}
+      />
+      <FormBigInput
+        register={register}
+        fieldId="differenceCompetitor"
+        fieldName="differenceCompetitor"
+        label="What's the difference between your project and closest competitor?"
+        type="big"
+        value={storageExampleUse.differenceCompetitor}
+        rows={6}
+      />
+      <FormBigInput
+        register={register}
+        fieldId="futurePromotions"
+        fieldName="futurePromotions"
+        label="Future promotions / sales / air drops?"
+        type="big"
+        value={storageExampleUse.futurePromotions}
+        rows={6}
+      />
+    </Form>
+  );
+};
+
+export default FeaturesTemplate;
